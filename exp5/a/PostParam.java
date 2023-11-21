640072b55c758f69b8419856f6a7c051477d63eb
Shiva Sankar <ms.shiva2019@gmail.com>
2023-11-21 19:33:11 +0530

    Delete exp5 directory

--- exp5/a/PostParam.java
+++
@@ -1,6 +0,0 @@
-public class PostParam extends GenericServlet {
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        PrintWriter pw = response.getParameterNames();
-    }
-
-}
